8046f43b52ea28758696324e7ce2163f087ec9e9
Hearty Oh <heartyoh@hatiolab.com>
2019-10-07 16:52:23 +0800

    Update attachment.ts

--- server/graphql/resolvers/attachment/attachment.ts
+++ server/graphql/resolvers/attachment/attachment.ts
@@ -2,9 +2,9 @@ import { getRepository } from 'typeorm'
 import { Attachment } from '../../../entities'
 
 export const attachmentResolver = {
-  async attachment(_: any, { name }, context: any) {
+  async attachment(_: any, { id }, context: any) {
     return await getRepository(Attachment).findOne({
-      where: { domain: context.state.domain, name, relations: ['domain', 'creator', 'updater'] }
+      where: { domain: context.state.domain, id, relations: ['domain', 'creator', 'updater'] }
     })
   }
 }
