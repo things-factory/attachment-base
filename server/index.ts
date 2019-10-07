9ef628be9b26a091cfc04986ccb6ba81a679fa6c
leemyunggyu <jaylee@hatiolab.com>
2019-10-07 14:41:51 +0900

    v0.0.21

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 export * from './entities'
-export * from './migrations'
 export * from './graphql'
+export { createAttachment } from './graphql/resolvers/attachment/create-attachment'
+export { createAttachments } from './graphql/resolvers/attachment/create-attachments'
+export * from './migrations'
 
 import './middlewares'
 import './routes'
