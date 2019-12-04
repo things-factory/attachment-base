93914e5080794d5a12f705ea2aaaac4532387203
Hearty Oh <heartyoh@hatiolab.com>
2019-12-04 21:09:32 +0800

    add gitignore user configuration file

--- config.development.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  attachmentsPath: 'attachments',
-  storage: {
-    type: 's3',
-    accessKeyId: 'AKIAUQEOPWEJEVICBDOD',
-    secretAccessKey: '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',
-    bucketName: 'opa-one'
-  }
-}
