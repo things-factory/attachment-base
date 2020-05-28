bcf057a12662030696bbd976689bcbf95f9a1a64
horwengliang <horwengliang@gmail.com>
2020-05-28 17:56:58 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -9,5 +9,6 @@ export { createAttachment } from './graphql/resolvers/attachment/create-attachme
 export { createAttachments } from './graphql/resolvers/attachment/create-attachments'
 export { deleteAttachment } from './graphql/resolvers/attachment/delete-attachment'
 export { deleteAttachmentsByRef } from './graphql/resolvers/attachment/delete-attachments-by-ref'
+export { multipleUpload, singleUpload } from './graphql/resolvers/attachment/upload'
 
 export * from './attachment-const'
